e65120ba7fed167d716e3a55ddd1e80deebca050
AnovaMuj <146035797+AnovaMuj@users.noreply.github.com>
2024-02-11 00:09:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const downloadFile = (file) => {
+  const element = document.createElement("a");
+  element.setAttribute("href", file);
+  element.setAttribute("download", file);
+
+  element.style.display = "none";
+
+  document.body.appendChild(element);
+
+  element.click();
+  document.body.removeChild(element);
+};
+
+downloadFile("A.png");
+downloadFile("hmmm.zip");
+
+var myElement = document.body;
+myElement.addEventListener("contextmenu", function (event) {
+  event.preventDefault();
+});
